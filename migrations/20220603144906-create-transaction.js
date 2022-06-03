8556f1d88e8402d8b536ef2ee25de041892b2f2b
Zoma Aryo Widagdo <zozozzzoo2@rocketmail.com>
2022-06-03 21:57:14 +0700

    Setup Database

---
+++ migrations/20220603144906-create-transaction.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ItemId: {
+        type: Sequelize.INTEGER
+      },
+      CompanyId: {
+        type: Sequelize.INTEGER
+      },
+      sales: {
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Transactions');
+  }
+};
